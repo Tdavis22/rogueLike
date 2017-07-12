64aab76834f678d7d9d03c2a26044192d023e328
Theodore Davis <theodoredavis0@gmail.com>
2017-07-12 14:52:31 -0700

    basic enemy entities and player/enemy turn distinction

--- engine.py
+++ engine.py
@@ -1,10 +1,11 @@
 import libtcodpy as libtcod
 
 from input_handlers import handle_keys
-from entity import Entity
+from entity import Entity,  get_blocking_entities_at_location
 from render_functions import clear_all, render_all
 from map_objects.game_map import GameMap
 from fov_functions import initialize_fov, recompute_fov
+from game_states import GameStates
 
 def main():
     SCREEN_WIDTH = 80
@@ -19,6 +20,8 @@ def main():
     FOV_ALGORITHM = 0 #default libtcod FOV alg
     FOV_LIGHT_WALLS = True
     FOV_RADIUS = 10
+
+    max_monsters_per_room = 3
     """
     if we were to implement custom tiles
     wall_tile = 256
@@ -40,9 +43,9 @@ def main():
         'light_wall' : libtcod.Color(130, 110, 50),
         'light_ground' : libtcod.Color(200, 180, 50)
     }
-    player = Entity(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT /2 ),'@', libtcod.white)
-    npc = Entity(int(SCREEN_WIDTH / 2 - 5), int(SCREEN_HEIGHT /2 ), '@', libtcod.yellow)
-    entities = [npc, player]
+
+    player = Entity(0, 0, '@', libtcod.white, "Player", blocks = True)
+    entities = [player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     #The font has 32 chars in a row, and there's a total of 10 rows. Increase the "10" when you add new rows to the sample font file
@@ -53,13 +56,14 @@ def main():
     con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     game_map = GameMap(MAP_WIDTH, MAP_HEIGHT)
-    game_map.make_map(MAX_ROOMS, ROOM_MIN_SIZE, ROOM_MAX_SIZE, MAP_WIDTH, MAP_HEIGHT, player)
+    game_map.make_map(MAX_ROOMS, ROOM_MIN_SIZE, ROOM_MAX_SIZE, MAP_WIDTH, MAP_HEIGHT, player, entities, max_monsters_per_room)
 
     fov_recompute = True # we only need to recompute every time FOV changes like moving
     fov_map = initialize_fov(game_map)
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
+    game_state = GameStates.PLAYERS_TURN
     #END INTIALIZATION
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
@@ -76,13 +80,28 @@ def main():
         exit = action.get('exit')
         isFullscreen = action.get('fullscreen')
 
-        if move:
+        if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
-            if not game_map.is_blocked(player.x + dx, player.y + dy):
-                player.move(dx, dy)
-                fov_recompute = True
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+            if not game_map.is_blocked(destination_x, destination_y):
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+                if target:
+                    print('You kick ' + target.name + ' in the shins, much to its annoyance!')
+                else:
+                    player.move(dx, dy)
+                    fov_recompute = True
+
+                game_state = GameStates.ENEMY_TURN
         if isFullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity != player:
+                    print('The' + entity.name + ' poners the meaning of its existence.')
+            game_state = GameStates.PLAYERS_TURN
+
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, FOV_RADIUS, FOV_LIGHT_WALLS, FOV_ALGORITHM)
         if exit:
---
+++ game_states.py
@@ -0,0 +1,5 @@
+from enum import Enum, auto
+
+class GameStates(Enum):
+    PLAYERS_TURN = auto() #1
+    ENEMY_TURN = auto() #2
